8c025455f16c23587a37ad9f7785e36f3f3429ba
Rohit Nimkar <nehalnimkar@gmail.com>
2023-06-26 18:39:04 +0530

    add shader red triangle

---
+++ glfw/shader/main.cpp
@@ -0,0 +1,132 @@
+/**
+ * @file    main.cpp
+ * @brief   Draw first triangle
+ * @author  Rohit Nimkar
+ * @date    06/25/2023
+ * @version 1.0
+ */
+#include <cstdio>
+#include <iostream>
+#include <GL/glew.h>
+
+#if defined(__APPLE__)
+    #include <OpenGL/gl.h>
+#else 
+    #include <GL/gl.h>
+#endif
+
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <ostream>
+#include "shader.h"
+
+int main(void)
+{
+    GLFWwindow* window    = nullptr;
+    GLenum rc             = 0;
+    GLint  n              = 0;
+    GLuint vertexBufferId = 0;
+	GLuint vertexArray    = 0;
+	GLuint vertexBuffer   = 0;
+    GLint result          = GL_FALSE;
+    GLuint program        = -1;
+
+	// Initialise GLFW
+	if( !glfwInit() )
+	{
+        std::cerr << "Failed to initialize glfw\n";
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow( 1024, 768, "Red triangle using Shaders", NULL, NULL);
+	if( window == NULL ){
+        std::cerr << "Failed to create window\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize glew\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	glGenVertexArrays(1, &vertexArray);
+	glBindVertexArray(vertexArray);
+
+	// Create and compile our GLSL program from the shaders
+	result = LoadShaders( "vertex.glsl", "fragment.glsl", &program);
+    if(GL_TRUE != result)
+    {
+        std::cerr << "Failed to link program\n";
+        return -1;
+    }
+
+	static const GLfloat vertexBufferData[] = { 
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f,
+	};
+
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexBufferData), vertexBufferData, GL_STATIC_DRAW);
+
+	do{
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(program);
+        std::cout << "Using program " << program << std::endl;
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(
+           0,                  
+           3,                  /* nElements per vertex */
+           GL_FLOAT,           /* type */
+           GL_FALSE,           /* OGL should not normalize data, as it is already normalized */
+           0,                  /* byte offset between generic vertex attributes */
+           (void*)0            /* offset to first vertex */
+        );
+
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+		glDisableVertexAttribArray(0);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 
+        );
+
+    /* release resources */
+	glDeleteBuffers(1, &vertexBuffer);
+	glDeleteVertexArrays(1, &vertexArray);
+	glDeleteProgram(program);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return (0);
+}
+
---
+++ glfw/shader/shader.cpp
@@ -0,0 +1,100 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+#include "shader.h"
+
+GLuint LoadShaders(const char * vertex_file_path, const char * fragment_file_path, GLuint *pProgram)
+{
+	// Create the shaders
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    GLint result;
+
+    result = loadShader(vertexShader, vertex_file_path);
+    if(GL_TRUE != result)
+    {
+        return (result);
+    }
+    result = loadShader(fragmentShader, fragment_file_path);
+    if(GL_TRUE != result)
+    {
+        std::cout << "Deleting vertex shader\n";
+        glDeleteShader(vertexShader);
+        std::cout << "Deleting vertex shader\n";
+        return (result);
+    }
+
+	// Link the program
+    std::cout << "Linking program\n";
+	*pProgram = glCreateProgram();
+	glAttachShader(*pProgram, vertexShader);
+	glAttachShader(*pProgram, fragmentShader);
+	glLinkProgram(*pProgram);
+
+	// Check the program
+	glGetProgramiv(*pProgram, GL_LINK_STATUS, &result);
+    if(GL_TRUE != result)
+    {
+        // linking Failed
+        int infoLogLen = 0;
+        std::cerr << "Failed to link program\nwooreogieogieroigeorni";
+        glGetProgramiv(*pProgram, GL_INFO_LOG_LENGTH, &infoLogLen);
+        if ( infoLogLen > 0 ){
+            char msg[infoLogLen + 1];
+            glGetProgramInfoLog(*pProgram, infoLogLen, nullptr, msg);
+            std::cerr << msg << std::endl;
+        }
+        return (result);
+    }
+	
+	glDetachShader(*pProgram, vertexShader);
+	glDetachShader(*pProgram, fragmentShader);
+	
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return (result);
+}
+
+GLint loadShader(GLuint shaderId, const char *pFilename)
+{
+    GLint result = GL_FALSE;
+	std::string sourceString;
+	std::ifstream sourceInputStream(pFilename, std::ios::in);
+
+	if(sourceInputStream.is_open()){
+		std::stringstream sstr;
+		sstr << sourceInputStream.rdbuf();
+		sourceString = sstr.str();
+		sourceInputStream.close();
+	}else{
+        std::cerr << "failed to read shader" << pFilename << std::endl;
+		return (GL_FALSE);
+	}
+
+	// Compile shader
+    std::cout << "Compiling shader: " << pFilename << std::endl;
+	char const * pSourceCode = sourceString.c_str();
+	glShaderSource(shaderId, 1, &pSourceCode , NULL);
+	glCompileShader(shaderId);
+
+    // validate compilation status
+	glGetShaderiv(shaderId, GL_COMPILE_STATUS, &result);
+    if(GL_TRUE != result)
+    {
+        int infoLogLen = 0;
+        std::cerr << "Failed to compile shader: " << pFilename << std::endl;
+
+        glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &infoLogLen);
+        if ( infoLogLen > 0 ){
+            char msg[infoLogLen];
+            glGetShaderInfoLog(shaderId, infoLogLen, NULL, msg);
+            std::cerr << msg << std::endl;
+        }
+    }
+    return (result);
+}
+
